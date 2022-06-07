248119436d71f2a103ef68df57c1b2f03c13fbc0
vasil <ip.vasiliy@bk.ru>
2022-06-07 21:31:48 +0400

    add base

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,6 @@
-﻿int numderA = 3;
+﻿int numderA = new Random().Next(1, 99);
 int numderB = 5;
 int result = numderA + numderB;
 Console.WriteLine(numderA + numderB); // прохой тон вычисления в выводе
 Console.WriteLine(result);
+Console.WriteLine(numderA);
