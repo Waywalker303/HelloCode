eb817c2bdd1d00b000afed8b32f4c4cbca642ce8
vasil <ip.vasiliy@bk.ru>
2022-06-07 20:06:20 +0400

    add example 002

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите имя");
+
+string username = Console.ReadLine(); // string - строка
+
+Console.WriteLine("Привет, " + username); // вывод в консоль * line - вывод в одну строку
+Console.Write("Беги! " + username); // вывод в консоль *вывод построчно
