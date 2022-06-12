b9d293c5b2f1c21ab3728491eaec5d34ed0d9e19
vasil <ip.vasiliy@bk.ru>
2022-06-12 16:18:46 +0400

    add homework 1 example

---
+++ Example001_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// создаёт случайное трёх значное число и выводит первый и последний знак
+int number = new Random().Next(100, 1000);
+
+Console.WriteLine(number);
+
+int firstDigit = number % 10; // 1 варинат
+int secondDigit = number / 100;
+Console.WriteLine(secondDigit * 10 + firstDigit); 
+
+int result = secondDigit * 10 + firstDigit; // 2й варинат
+Console.WriteLine(result); 
