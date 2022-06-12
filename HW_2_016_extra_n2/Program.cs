9df2c0e2d0a1243da5e2faa4d52a360cf8ae1f64
vasil <ip.vasiliy@bk.ru>
2022-06-12 20:51:18 +0400

    add homework 2 extra example

---
+++ HW_2_016_extra_n2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 16 Напишите программу которая принимает на вход два числа и проверяет являеться ли одно квадратом другого
+
+int numberOne = Input("Введите первое число: ");
+int numberTwo = Input("Введите первое число: ");
+
+if (numberOne * numberOne == numberTwo)
+{
+    Console.WriteLine("Да! Число: " + numberTwo + " являеться квадратом числа: " + numberOne);
+}
+else
+{
+    Console.WriteLine("Нет! Число: " + numberTwo + " не являеться квадратом числа: " + numberOne);
+}
+
+int Input(string output)
+{
+    Console.WriteLine(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
