772ebce8ca61682884ce4d2d11377438f2b2e345
vasil <ip.vasiliy@bk.ru>
2022-06-16 21:29:17 +0400

    add example 4

---
+++ Example_4_026_znak/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 26  Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5*/
+
+Console.WriteLine("Enter your number: ");
+int numb = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (numb >= 10)
+{
+    numb = numb / 10;
+    count ++;
+} 
+Console.WriteLine(count);
