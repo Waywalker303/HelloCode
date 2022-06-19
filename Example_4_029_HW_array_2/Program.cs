024224be70e5e17574b6d3b6ebfe53ec8a54ded5
vasil <ip.vasiliy@bk.ru>
2022-06-19 10:37:13 +0400

    add homework example 4

---
+++ Example_4_029_HW_array_2/Program.cs
@@ -0,0 +1,76 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Вывод сделать отдельным методом.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] */
+/*
+int[] array = new int[8];
+int[] array = {1, 2, 19};
+
+PrintArray(array);
+
+
+void PrintArray(int[] col)
+{
+    int count = array.Length;
+    int position = 0;
+    Console.Write("[ ");
+    while (position < count)
+    {
+        Console.Write($"{array[position]}{" "}");
+        position++;
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+
+
+
+/*
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
+*/
